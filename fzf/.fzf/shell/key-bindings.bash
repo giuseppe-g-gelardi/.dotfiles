40b5056678a3081cd7257b1dab049ffd39d9220e
giuseppe-g-gelardi <giuseppegelardi@icloud.com>
2023-11-28 21:33:46 -0500

    update vscode profile stuff, slowly narrow down and start to remove fzf stuff, change hypr config to open tidal with a different keybind (was conflicting with movements), actually make tmux-windowizer executable. update sessionizer to open new session with fzf within tmux and windowizer opens a new window in the current session. HALLELUJAH

--- fzf/.fzf/shell/key-bindings.bash
+++ fzf/.fzf/shell/key-bindings.bash
@@ -8,25 +8,6 @@
 
 # Key bindings
 # ------------
-# - this function will find and cd into a directory and open neovim
-# - both commands are executed in a subshell and returns to the 
-# - original directory when neovim is closed
-# __fzf_cd_and_open_nvim__() {
-#   local dir
-#   dir=$(find . \( -type d -o -name '.*' \) 2>/dev/null | fzf +m --preview "tree -L 1 {}")
-#   if [ -n "$dir" ]; then
-#     (cd "$dir" && nvim .)
-#   fi
-# }
-#
-# __fzf_cd_and_open_code__() {
-#   local dir
-#   dir=$(find . \( -type d -o -name '.*' \) 2>/dev/null | fzf +m --preview "tree -L 1 {}")
-#   if [ -n "$dir" ]; then
-#     (cd "$dir" && code .)
-#   fi
-# }
-#
 # - this function will find and cd into a directory
 __fzf_cd_into__() {
   local dir
@@ -39,15 +20,6 @@ __fzf_cd_into__() {
   fi
 }
 
-# # Required to refresh the prompt after fzf
-# bind -m emacs-standard '"\er": redraw-current-line'
-
-# # CTRL-G - cd into the selected directory and open neovim
-# bind -x '"\C-g": __fzf_cd_and_open_nvim__'
-#
-# # CTRL-E - cd into the selected directory and open vscode
-# bind -x '"\C-e": __fzf_cd_and_open_code__'
-
 # CTRL-F - cd into the selected directory
-bind -x '"\C-f": __fzf_cd_into__'
+bind -x '"\C-g": __fzf_cd_into__'
 
--- fzf/.fzf/shell/key-bindings.zsh
+++ fzf/.fzf/shell/key-bindings.zsh
@@ -17,28 +17,6 @@
 # Key bindings
 # ------------
 
-# # Function to find and cd into a directory and open neovim
-# __fzf_cd_and_open_nvim__() {
-#   local dir
-#   dir=$(find . \( -type d -o -name '.*' \) 2>/dev/null | fzf +m --preview "tree -L 1 {}")
-#   if [ -n "$dir" ]; then
-#     zle reset-prompt  # Clear the current input line
-#     zle accept-line   # Automatically accept the FZF selection
-#     (cd "$dir" && nvim . -c "e .")
-#   fi
-# }
-#
-# # Function to find and cd into a directory and open VS Code
-# __fzf_cd_and_open_code__() {
-#   local dir
-#   dir=$(find . \( -type d -o -name '.*' \) 2>/dev/null | fzf +m --preview "tree -L 1 {}")
-#   if [ -n "$dir" ]; then
-#     zle reset-prompt  # Clear the current input line
-#     zle accept-line   # Automatically accept the FZF selection
-#     (cd "$dir" && code .)
-#   fi
-# }
-#
 # Function to find and cd into a directory
 __fzf_cd_into__() {
   local dir
@@ -47,14 +25,10 @@ __fzf_cd_into__() {
     zle reset-prompt  # Clear the current input line
     zle accept-line   # Automatically accept the FZF selection
     cd "$dir"
-    print "cd --> $dir"
   fi
 }
 
 # Bindings for Zsh
-# zle -N __fzf_cd_and_open_nvim__
-# zle -N __fzf_cd_and_open_code__
 zle -N __fzf_cd_into__
-# bindkey '^G' __fzf_cd_and_open_nvim__
-# bindkey '^E' __fzf_cd_and_open_code__
 bindkey '^T' __fzf_cd_into__
+
